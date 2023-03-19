586053adb1f7a18604244c2309d0b02d9bce7cf0
Gustavo Trapp <101607291+gutrapp@users.noreply.github.com>
2023-03-19 16:49:16 -0300

    Update connection.go

--- server/models/connection.go
+++ server/models/connection.go
@@ -10,7 +10,7 @@ import (
 var db *gorm.DB
 
 func Setup() {
-	dsn := "host=localhost user=postgres password=99831745gusta dbname=ownboss port=5432"
+	dsn := "host=[] user=[] password=[] dbname=[] port=[]"
 	var err error
 	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
